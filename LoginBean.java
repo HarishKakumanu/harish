a043b9a2902e640419d008042658198ecc637483
HarishKakumanu <31705122+HarishKakumanu@users.noreply.github.com>
2017-09-06 18:15:53 -0500

    Add files via upload

---
+++ LoginBean.java
@@ -0,0 +1,32 @@
+package com.harish.bean;
+
+public class LoginBean {
+private int uNO;
+public int getuNO() {
+	return uNO;
+}
+public void setuNO(int uNO) {
+	this.uNO = uNO;
+}
+public String getuName() {
+	return uName;
+}
+public void setuName(String uName) {
+	this.uName = uName;
+}
+public String getuPass() {
+	return uPass;
+}
+public void setuPass(String uPass) {
+	this.uPass = uPass;
+}
+public String getEmail() {
+	return Email;
+}
+public void setEmail(String email) {
+	Email = email;
+}
+private String uName;
+private String uPass;
+private String Email;
+}
